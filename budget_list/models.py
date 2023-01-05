10167fc4168bab7109d685b329e4c1251c6ea4ab
Martin Wanczyk <wanczyk.martin@gmail.com>
2023-01-05 20:29:15 +0100

    Create models

--- budget_list/models.py
+++ budget_list/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class BudgetList(models.Model):
+    participants = models.ManyToManyField(User, related_name='+')
+
+
+class Budget(models.Model):
+    budget_list = models.ForeignKey(BudgetList, on_delete=models.CASCADE, related_name="budgets")
+    name = models.CharField(max_length=50)
+
+
+class Income(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE, related_name="incomes")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    amount = models.DecimalField(decimal_places=2, max_digits=6)
+
+
+class Expense(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE, related_name="expenses")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    amount = models.DecimalField(decimal_places=2, max_digits=6)
