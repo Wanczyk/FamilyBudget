a634806ecda0018e778587129cb8cb7a7022c3a3
Martin Wanczyk <wanczyk.martin@gmail.com>
2023-01-05 19:30:54 +0100

    Switch to JWT authentication

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 SITE_ID = 1
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
+    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10
 }
+
+REST_USE_JWT = True
+
+JWT_AUTH_COOKIE = 'family-budget-auth'
+JWT_AUTH_REFRESH_COOKIE = 'family-budget-refresh-token'
