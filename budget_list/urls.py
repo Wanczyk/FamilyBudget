86ccf3c569126b367e9cafd8cd45ffb0e0c4cd85
Martin Wanczyk <wanczyk.martin@gmail.com>
2023-01-06 15:18:47 +0100

    Use nested router

--- budget_list/urls.py
+++ budget_list/urls.py
@@ -1,14 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
+
 from budget_list import views
 
 
 router = DefaultRouter()
 router.register("budgetlists", views.BudgetListViewSet, basename="budgetlist")
-router.register("budgets", views.BudgetViewSet, basename="budget")
-router.register("incomes", views.IncomeViewSet, basename="income")
-router.register("expenses", views.ExpenseViewSet, basename="expense")
+
+budgetlist_router = routers.NestedSimpleRouter(router, r'budgetlists', lookup='budgetlist')
+budgetlist_router.register("budgets", views.BudgetViewSet, basename="budget")
+
+budget_router = routers.NestedSimpleRouter(budgetlist_router, r'budgets', lookup='budget')
+budget_router.register(r'incomes', views.IncomeViewSet, basename='income')
+budget_router.register(r'expenses', views.ExpenseViewSet, basename='expense')
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path(r'', include(router.urls)),
+    path(r'', include(budgetlist_router.urls)),
+    path(r'', include(budget_router.urls)),
 ]
